e3efd70034c7537b9dfc70dc9260212acb36b83d
Gareth Miller <gmiller290488@gmail.com>
2017-04-05 18:52:21 +0100

    Changed to show new blog posts first

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -22,7 +22,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
