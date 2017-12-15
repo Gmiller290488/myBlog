f2908e862c99b2d439f0a581c5e8e71fe2b2f405
gmiller290488 <gmiller290488@gmail.com>
2017-12-15 16:55:50 +0000

    added tags

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    tags = TaggableManager()
 
 
     def publish(self):
